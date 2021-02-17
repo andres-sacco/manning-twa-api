1100b62bb9f42014374655105499f79ee187a718
asacco <sacco.andres@gmail.com>
2021-02-16 23:19:37 -0300

    feat: add docker-compose file working

---
+++ api-pricing/src/test/java/com/twa/flights/api/pricing/configuration/SwaggerConfigurationTest.java
@@ -0,0 +1,31 @@
+package com.twa.flights.api.pricing.configuration;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.twa.flights.api.pricing.service.AppInfoService;
+
+public class SwaggerConfigurationTest {
+
+    AppInfoService appInfoService;
+
+    @BeforeEach
+    public void setUp() {
+        appInfoService = mock(AppInfoService.class);
+    }
+
+    @Test
+    public void should_return_information_of_default_api() {
+        SwaggerConfiguration swaggerConfiguration = new SwaggerConfiguration(appInfoService);
+        assertNotNull(swaggerConfiguration.defaultApi());
+    }
+
+    @Test
+    public void should_return_information_of_openapi() {
+        SwaggerConfiguration swaggerConfiguration = new SwaggerConfiguration(appInfoService);
+        assertNotNull(swaggerConfiguration.openAPI());
+    }
+}
---
+++ api-provider-beta/src/test/java/com/twa/flights/api/provider/beta/controller/DocumentationControllerTest.java
@@ -0,0 +1,44 @@
+package com.twa.flights.api.provider.beta.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+public class DocumentationControllerTest {
+
+    private DocumentationController controller;
+    private HttpServletResponse response;
+
+    @BeforeEach
+    public void setUp() {
+        response = mock(HttpServletResponse.class);
+        controller = new DocumentationController();
+    }
+
+    @Test
+    public void should_redirect_documentation_without_error() {
+        controller.redirectToDocumentation(response);
+    }
+
+    @Test
+    public void should_redirect_documentation_error() throws IOException {
+        doThrow(new IOException()).when(response).sendRedirect(anyString());
+        controller.redirectToDocumentation(response);
+    }
+
+    @Test
+    public void should_redirect_documentation_error_message() throws IOException {
+        doThrow(new IOException("as")).when(response).sendRedirect(anyString());
+        controller.redirectToDocumentation(response);
+    }
+
+}

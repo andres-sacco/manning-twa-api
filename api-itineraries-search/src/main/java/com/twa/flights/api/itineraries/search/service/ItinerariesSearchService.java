d69546f1e03d4064254a7a78b7da454826df9447
asacco <sacco.andres@gmail.com>
2021-03-02 20:55:57 -0300

    feat: increase the latency in the providers

--- api-itineraries-search/src/main/java/com/twa/flights/api/itineraries/search/service/ItinerariesSearchService.java
+++ api-itineraries-search/src/main/java/com/twa/flights/api/itineraries/search/service/ItinerariesSearchService.java
@@ -11,8 +11,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 
 import com.google.common.collect.Lists;
+import com.twa.flights.api.itineraries.search.exception.TWAException;
 import com.twa.flights.api.itineraries.search.facade.ProviderFacade;
 import com.twa.flights.api.itineraries.search.helper.FlightDuplicationCheckHelper;
 import com.twa.flights.common.dto.itinerary.ItineraryDTO;
@@ -72,11 +74,11 @@ public class ItinerariesSearchService {
             try {
                 rawItineraries = futureFlightsList.get();
                 flights.addAll(rawItineraries);
+            } catch (TWAException | ExecutionException | WebClientRequestException e) {
+                LOGGER.error(e.getMessage(), e);
             } catch (InterruptedException e) {
                 LOGGER.warn("Interrupted!", e);
                 Thread.currentThread().interrupt();
-            } catch (ExecutionException e) {
-                LOGGER.error(e.getMessage(), e);
             }
         }
     }

f44dd534f47fe6d39c3854387ec3c091a3cc9cf4
fernando-bernardino <tony.bernardino@transferwise.com>
2021-05-16 15:27:12 +0100

    api-provider-alpha + api-provider-beta fully configured.

---
+++ api-provider-alpha/src/main/java/com/twa/flights/api/provider/alpha/configuration/RedisConfiguration.java
@@ -0,0 +1,50 @@
+package com.twa.flights.api.provider.alpha.configuration;
+
+import com.twa.flights.api.provider.alpha.configuration.settings.RedisSettings;
+import com.twa.flights.api.provider.alpha.dto.CityDTO;
+import com.twa.flights.api.provider.alpha.serializer.CitySerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.*;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@ConfigurationProperties
+public class RedisConfiguration {
+
+    private RedisSettings redis;
+
+    private CitySerializer citySerializer;
+
+    @Autowired
+    public RedisConfiguration(CitySerializer citySerializer) {
+        this.citySerializer = citySerializer;
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redis.getHost(),
+                redis.getPort());
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, CityDTO> redisCityTemplate() {
+        RedisTemplate<String, CityDTO> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+
+        redisTemplate.setValueSerializer(citySerializer);
+
+        return redisTemplate;
+    }
+
+    public RedisSettings getRedis() {
+        return redis;
+    }
+
+    public void setRedis(RedisSettings redis) {
+        this.redis = redis;
+    }
+}
---
+++ api-provider-alpha/src/main/java/com/twa/flights/api/provider/alpha/configuration/settings/CacheConfiguration.java
@@ -0,0 +1,27 @@
+package com.twa.flights.api.provider.alpha.configuration.settings;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.*;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Data
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "cache")
+@Configuration
+public class CacheConfiguration {
+    private Map<String, CacheSettings> settings;
+
+    public CacheSettings getCacheSettings(String settings) {
+        return this.settings.get(settings);
+    }
+
+    public Map<String, CacheSettings> getSettings() {
+        return settings;
+    }
+
+    public void setSettings(Map<String, CacheSettings> settings) {
+        this.settings = settings;
+    }
+}

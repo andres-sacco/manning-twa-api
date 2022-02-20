88070cd2e927732d6ce1e0c9d67edd81b07c6065
cristianregnier <cristianregnier@gmail.com>
2022-02-20 09:02:47 -0300

    Cache service that fetch cities

--- api-clusters/src/main/java/com/twa/flights/api/clusters/service/CatalogService.java
+++ api-clusters/src/main/java/com/twa/flights/api/clusters/service/CatalogService.java
@@ -1,12 +1,12 @@
 package com.twa.flights.api.clusters.service;
 
+import com.twa.flights.api.clusters.connector.CatalogConnector;
+import com.twa.flights.api.clusters.dto.CityDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
-import com.twa.flights.api.clusters.connector.CatalogConnector;
-import com.twa.flights.api.clusters.dto.CityDTO;
 
 @Service
 public class CatalogService {
@@ -20,6 +20,7 @@ public class CatalogService {
         this.catalogConnector = catalogConnector;
     }
 
+    @Cacheable(value = "cities")
     public CityDTO getCity(String code) {
         LOGGER.debug("Obtain the information for code: {}", code);
         return catalogConnector.getCityByCode(code);

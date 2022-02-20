2140c0c5eda0a5eed6006dd020c60a01226b4e99
cristianregnier <cristianregnier@gmail.com>
2022-02-19 21:27:20 -0300

    Implement Caffeine as cache manager

---
+++ api-clusters/src/main/java/com/twa/flights/api/clusters/configuration/CacheManagerConfiguration.java
@@ -0,0 +1,38 @@
+package com.twa.flights.api.clusters.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+@EnableConfigurationProperties(CacheConfiguration.class)
+public class CacheManagerConfiguration {
+
+   private final CacheConfiguration cacheConfiguration;
+
+   public CacheManagerConfiguration(CacheConfiguration cacheConfiguration) {
+      this.cacheConfiguration = cacheConfiguration;
+   }
+
+   @Bean
+   public Caffeine caffeineConfig() {
+      return Caffeine.newBuilder()
+              .expireAfterWrite(cacheConfiguration.getDuration(), TimeUnit.MINUTES)
+              .maximumSize(cacheConfiguration.getMaximumSize());
+   }
+
+   @Bean
+   public CacheManager cacheManager(Caffeine caffeine) {
+      var caffeineCacheManager = new CaffeineCacheManager();
+      caffeineCacheManager.setCaffeine(caffeine);
+
+      return caffeineCacheManager;
+   }
+}

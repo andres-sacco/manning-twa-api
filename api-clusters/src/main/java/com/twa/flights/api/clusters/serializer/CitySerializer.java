c23baabb144c6499acfdda26c86a6c0e57181f84
cristianregnier <cristianregnier@gmail.com>
2022-02-26 11:25:03 -0300

    Implement Redis's class to serialize/deserialize city objects

---
+++ api-clusters/src/main/java/com/twa/flights/api/clusters/serializer/CitySerializer.java
@@ -0,0 +1,21 @@
+package com.twa.flights.api.clusters.serializer;
+
+import com.twa.flights.api.clusters.dto.CityDTO;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CitySerializer implements RedisSerializer<CityDTO> {
+
+    @Override
+    public byte[] serialize(CityDTO cityDTO) throws SerializationException {
+        return JsonSerializer.serialize(cityDTO);
+    }
+
+    @Override
+    public CityDTO deserialize(byte[] bytes) throws SerializationException {
+        return JsonSerializer.deserialize(bytes, CityDTO.class);
+    }
+
+}

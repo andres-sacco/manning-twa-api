b672096636d7f014e22e5024cb077c86e3e8a1ec
cristianregnier <cristianregnier@gmail.com>
2022-02-26 13:05:08 -0300

    Create new cache settings class

---
+++ api-clusters/src/main/java/com/twa/flights/api/clusters/configuration/settings/CacheSettings.java
@@ -0,0 +1,18 @@
+package com.twa.flights.api.clusters.configuration.settings;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConfigurationProperties(prefix = "cache")
+@ConstructorBinding
+public class CacheSettings {
+    private final long expireAfterWriteTime;
+
+    public CacheSettings(long expireAfterWriteTime) {
+        this.expireAfterWriteTime = expireAfterWriteTime;
+    }
+
+    public long getExpireAfterWriteTime() {
+        return expireAfterWriteTime;
+    }
+}

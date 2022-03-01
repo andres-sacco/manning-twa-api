bd56ba888026b766dad2c2a4d3e2e601f19ed126
cristianregnier <cristianregnier@gmail.com>
2022-03-01 14:43:11 -0300

    Add a gzip string compressor

---
+++ api-provider-beta/src/main/java/com/twa/flights/api/provider/beta/connector/filter/CompressionException.java
@@ -0,0 +1,7 @@
+package com.twa.flights.api.provider.beta.connector.filter;
+
+public class CompressionException extends Exception {
+   public CompressionException(String message, Throwable cause) {
+      super(message, cause);
+   }
+}
